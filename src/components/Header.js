3352ca82abb5432c03f9913baec4fe7d7f354624
Ekram Hosen <ekramhosen329@gmail.com>
2022-09-11 17:11:04 +0600

    react icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FiMenu } from "react-icons/fi";
 
 const Header = () => {
   return (
@@ -8,20 +9,7 @@ const Header = () => {
           <div className="navbar-start">
             <div className="dropdown">
               <label className="btn btn-ghost lg:hidden">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />
-                </svg>
+                <FiMenu className="h-5 w-5" />
               </label>
               <ul className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                 <li>
