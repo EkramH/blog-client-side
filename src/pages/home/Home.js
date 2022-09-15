34c2083af51cefa4552aa09819a9bfecd715af74
Ekram Hosen <ekramhosen329@gmail.com>
2022-09-15 12:42:19 +0600

    blog card

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,13 +8,24 @@ const Home = () => {
       .then((data) => setBlogs(data));
   }, []);
 
-  console.log(blogs);
   return (
     <div className="container mx-auto px-6 my-32">
-      <h2 className="text-2xl font-semibold">All Blogs...</h2>
+      <h2 className="text-2xl font-semibold mb-6">All Blogs...</h2>
       {blogs.map((blog) => (
-        <div>
-          <h1>{blog.title}</h1>
+        <div key={blog._id}>
+          <div className="card lg:card-side shadow-[0px_0px_20px_-5px_rgba(0,0,0,0.2)] lg:w-3/4 mx-auto my-10">
+            <figure>
+              <img
+                src={blog.imageURL}
+                alt="Album"
+                className="h-72 shadow-[0px_0px_20px_-5px_rgba(0,0,0,0.2)]"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{blog.title}</h2>
+              <p>blog.blog</p>
+            </div>
+          </div>
         </div>
       ))}
     </div>
